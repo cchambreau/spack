93465de8c413c432d4e45a98c1b41f8610d7fe64
Todd Gamblin <tgamblin@llnl.gov>
2020-10-26 11:06:03 -0700

    sbang: convert sbang script to POSIX shell
    `sbang` was previously a bash script but did not need to be. This
    converts it to a plain old POSIX shell script and adds some options. This
    also allows us to simplify sbang shebangs to `#!/bin/sh /path/to/sbang`
    instead of `#!/bin/bash /path/to/sbang`.
    
    The new script passes shellcheck (with a few exceptions noted in the file)
    
    - [x] `SBANG_DEBUG` env var enables printing what *would* be executed
    - [x] `sbang` checks whether it has been passed an option and fails gracefully
    - [x] `sbang` will now fail if it can't find a second shebang line, or if
          the second line happens to be sbang (avoid infinite loops)
    - [x] add more rigorous tests for `sbang` behavior using `SBANG_DEBUG`

--- lib/spack/spack/test/sbang.py
+++ lib/spack/spack/test/sbang.py
@@ -23,18 +23,21 @@ from spack.util.executable import which
 
 short_line        = "#!/this/is/short/bin/bash\n"
 long_line         = "#!/this/" + ('x' * 200) + "/is/long\n"
+
 lua_line          = "#!/this/" + ('x' * 200) + "/is/lua\n"
 lua_in_text       = ("line\n") * 100 + "lua\n" + ("line\n" * 100)
 lua_line_patched  = "--!/this/" + ('x' * 200) + "/is/lua\n"
+
 node_line         = "#!/this/" + ('x' * 200) + "/is/node\n"
 node_in_text      = ("line\n") * 100 + "lua\n" + ("line\n" * 100)
 node_line_patched = "//!/this/" + ('x' * 200) + "/is/node\n"
-sbang_line        = '#!/bin/bash %s/bin/sbang\n' % spack.store.layout.root
+
 php_line         = "#!/this/" + ('x' * 200) + "/is/php\n"
 php_in_text      = ("line\n") * 100 + "php\n" + ("line\n" * 100)
 php_line_patched = "<?php #!/this/" + ('x' * 200) + "/is/php\n"
 php_line_patched2 = "?>\n"
-sbang_line        = '#!/bin/bash %s/bin/sbang\n' % spack.store.layout.root
+
+sbang_line        = '#!/bin/sh %s/bin/sbang\n' % spack.store.layout.root
 last_line         = "last!\n"
 
 
@@ -178,7 +181,7 @@ def test_shebang_handles_non_writable_files(script_dir):
     assert oct(not_writable_mode) == oct(st.st_mode)
 
 
-def check_sbang():
+def check_sbang_installation():
     sbang_path = sbang.sbang_install_path()
     sbang_bin_dir = os.path.dirname(sbang_path)
     assert sbang_path.startswith(spack.store.layout.root)
@@ -201,7 +204,7 @@ def test_install_sbang(install_mockery):
     assert not os.path.exists(sbang_bin_dir)
 
     sbang.install_sbang()
-    check_sbang()
+    check_sbang_installation()
 
     # put an invalid file in for sbang
     fs.mkdirp(sbang_bin_dir)
@@ -209,8 +212,71 @@ def test_install_sbang(install_mockery):
         f.write("foo")
 
     sbang.install_sbang()
-    check_sbang()
+    check_sbang_installation()
 
     # install again and make sure sbang is still fine
     sbang.install_sbang()
-    check_sbang()
+    check_sbang_installation()
+
+
+def test_sbang_fails_without_argument():
+    sbang = which(spack.paths.sbang_script)
+    sbang(fail_on_error=False)
+    assert sbang.returncode == 1
+
+
+def test_sbang_with_different_shebangs(tmpdir):
+    script = str(tmpdir.join("script"))
+
+    def check_sbang(shebang, returncode, expected=None):
+        # write a script out with <shebang> on second line
+        with open(script, "w") as f:
+            f.write("#!/bin/sh {sbang}\n{shebang}\n".format(
+                sbang=spack.paths.sbang_script,
+                shebang=shebang
+            ))
+        fs.set_executable(script)
+
+        # test running the script in debug, which prints what would be executed
+        exe = which(script)
+        out = exe(output=str, fail_on_error=False, env={"SBANG_DEBUG": "1"})
+
+        # check error status and output vs. expected
+        assert exe.returncode == returncode
+
+        if expected is not None:
+            assert expected == out.strip()
+
+    # perl, with and without /usr/bin/env
+    check_sbang("#!/path/to/perl", 0, "/path/to/perl -x %s" % script)
+    check_sbang("#!/usr/bin/env perl", 0, "/usr/bin/env perl -x %s" % script)
+
+    # perl -w, with and without /usr/bin/env
+    check_sbang("#!/path/to/perl -w", 0, "/path/to/perl -w -x %s" % script)
+    check_sbang(
+        "#!/usr/bin/env perl -w", 0, "/usr/bin/env perl -w -x %s" % script)
+
+    # ruby, with and without /usr/bin/env
+    check_sbang("#!/path/to/ruby", 0, "/path/to/ruby -x %s" % script)
+    check_sbang("#!/usr/bin/env ruby", 0, "/usr/bin/env ruby -x %s" % script)
+
+    # python, with and without /usr/bin/env
+    check_sbang("#!/path/to/python", 0, "/path/to/python %s" % script)
+    check_sbang("#!/usr/bin/env python", 0, "/usr/bin/env python %s" % script)
+
+    # php with one-line php comment
+    check_sbang("<?php #!/usr/bin/php ?>", 0, "/usr/bin/php %s" % script)
+
+    # simple shell scripts
+    check_sbang("#!/bin/sh", 0, "/bin/sh %s" % script)
+    check_sbang("#!/bin/bash", 0, "/bin/bash %s" % script)
+
+    # error case: sbang as infinite loop
+    check_sbang("#!/path/to/sbang", 1)
+    check_sbang("#!/usr/bin/env sbang", 1)
+
+    # lua
+    check_sbang("--!/path/to/lua", 0, "/path/to/lua %s" % script)
+
+    # node
+    check_sbang("//!/path/to/node", 0, "/path/to/node %s" % script)
